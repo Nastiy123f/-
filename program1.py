c650e4f55983f228802614ace9f332412e9217c2
Nastiy123f <160404266+Nastiy123f@users.noreply.github.com>
2024-02-18 15:40:01 +0200

    Create program1.py

---
+++ program1.py
@@ -0,0 +1,17 @@
+score = 0
+a = int(input("Чому дорівнює різниця добутку 5 і 4 і числа 10?"))
+b = int(input("Чому дорівнює сума частки 12 і 2 і числа 11"))
+c = int(input("Чому дорівнює частка суми 5 і 2 і числа 7"))
+if a ==10:
+    score+=1
+else:
+    score+=0
+if b == 17:
+    score+=1
+else:
+    score+=0
+if c==1:
+    score+=1
+else:
+    score+=0
+print("Результат:", score, "з 3")
